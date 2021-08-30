a65eb9103ac07fc983a6592fd4bcf1b16d435674
luhem <luhem@DESKTOP-OLOTUN5>
2021-08-30 17:30:40 -0500

    g

--- src/co/edu/unbosque/model/Pet.java
+++ src/co/edu/unbosque/model/Pet.java
@@ -26,3 +26,61 @@ public class Pet {
 		return auxPet;
 	}
 
+	
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public long getMicrochip() {
+		return microchip;
+	}
+
+	public void setMicrochip(long microchip) {
+		this.microchip = microchip;
+	}
+
+	public String getSpecies() {
+		return species;
+	}
+
+	public void setSpecies(String species) {
+		this.species = species;
+	}
+
+	public String getSex() {
+		return sex;
+	}
+
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+
+	public String getSize() {
+		return size;
+	}
+
+	public void setSize(String size) {
+		this.size = size;
+	}
+
+	public boolean getPotentDangerous() {
+		return potentDangerous;
+	}
+
+	public void setPotentDangerous(boolean potentDangerous) {
+		this.potentDangerous = potentDangerous;
+	}
+
+	public String getNeighborhood() {
+		return neighborhood;
+	}
+
+	public void setNeighborhood(String neighborhood) {
+		this.neighborhood = neighborhood;
+	}
+
+}
