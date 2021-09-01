4eb9c9cc5d92726e74a607d930c86775f222453a
luhem <luhem@DESKTOP-OLOTUN5>
2021-09-01 10:06:17 -0500

    menu

--- src/co/edu/unbosque/controller/Controller.java
+++ src/co/edu/unbosque/controller/Controller.java
@@ -1,6 +1,7 @@
 package co.edu.unbosque.controller;
 
 import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 import co.edu.unbosque.model.Manager;
 
@@ -24,6 +25,21 @@ public class Controller {
 		}
 	}
 	public void menu() {
+		String continueMenu = "Y";
+		System.out.println("-------------------------------------");
+		System.out.println("      WELCOME TO PETS MANAGER");
 		
+		while(continueMenu.equalsIgnoreCase("y")) {
+			System.out.println("-------------------------------------");
+			System.out.println("              MENU");
+			System.out.println("-------------------------------------");
+			System.out.println("Please type the option number:");
+			System.out.println("1.Find Pet By Microchip.");
+			System.out.println("2.Count Pet By Species.");
+			System.out.println("3.Count Pet By Neighborhood.");
+			System.out.println("4.Find by multiple fields.");
+			System.out.println("5.Exit.");
+			Scanner sc = new Scanner(System.in);
+			String option=sc.next();
 	}
 }
