b52c21cec2e93c2c9ccc520809bd6ab57d5ee7a0
luhem <luhem@DESKTOP-OLOTUN5>
2021-09-01 09:49:12 -0500

    c

--- src/co/edu/unbosque/controller/Controller.java
+++ src/co/edu/unbosque/controller/Controller.java
@@ -1,11 +1,29 @@
 package co.edu.unbosque.controller;
 
+import java.io.FileNotFoundException;
+
 import co.edu.unbosque.model.Manager;
 
 public class Controller {
 	Manager m;
-	
+
 	public Controller() {
-	
+		try {
+
+			m = new Manager();
+			m.uploadData();
+			System.out.println("Assigning Ids...");
+			m.assignID();
+
+			// showPetsInfo();
+			menu();
+
+		} catch (FileNotFoundException e) {
+			System.out.println("No encontré el archivo");
+			e.printStackTrace();
+		}
+	}
+	public void menu() {
+		
 	}
 }
