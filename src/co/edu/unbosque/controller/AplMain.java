52328815262687a8df6b0f3b113bf9310ade0ceb
luhemapo <82292328+luhemapo@users.noreply.github.com>
2021-09-01 20:43:47 -0500

    Update AplMain.java

--- src/co/edu/unbosque/controller/AplMain.java
+++ src/co/edu/unbosque/controller/AplMain.java
@@ -1,7 +1,18 @@
 package co.edu.unbosque.controller;
+/**
+ * Clase Principal Main 
+ * @author Luis Martinez, Andres Carbonell
+ * @version 5.0
+ * @serial 2021
+ *
+ */
 
 public class AplMain {
-
+	/**
+	 * Metodo principal que llama al controlador
+	 * 
+	 */
+	
 	public static void main(String[] args) {
 		Controller c= new Controller();
 
